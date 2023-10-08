9229d1b40350298f5859e9d282a62460515de5f6
ZXD318 <djuricz@oregonstate.edu>
2023-10-08 09:14:14 -0500

    This is just the beginning of the project and semester. No real changes have been made at this point.

---
+++ average.py
@@ -0,0 +1,15 @@
+# Python program to find the average of five numbers
+
+# take inputs
+
+num1 = -2.4
+num2 = 5.1
+num3 = 6.0
+num4 = 123.8
+num5 = -19.0
+
+# calculate average
+avg = (num1 + num2 + num3 + num4 + num5) / 5
+
+# print average value
+print('he average of numbers = %0.2f' %avg)
