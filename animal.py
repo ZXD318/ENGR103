5191d39ca0c6ccc4c6da0fef38c7e0566f79de75
ZXD318 <djuricz@oregonstate.edu>
2023-10-08 09:33:24 -0500

    Project 2.a

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Auther: Zoran Djuric
+# GitHub username: ZXD318
+# Date: 09/29/2023
+# Description: Asks the user for their favorite animal then prints out that is their favorite animal.
+
+fave_animal = input('Please enter your favorite animal: ')
+print("Your favorite animal is the " + fave_animal + ".")
